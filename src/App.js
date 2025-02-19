2a99c077b37c95dfe10e0a3401ce293633020c38
Himanshu <chalpehimanshu17@gmail.com>
2025-02-20 02:33:49 +0530

    Initial commit - RPG Portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Game from "./Game";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="text-2xl font-bold text-center mt-4">
+        Himanshu's RPG Portfolio
+      </h1>
+      <Game />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Game.js
@@ -0,0 +1,39 @@
+import Phaser from "phaser";
+import React, { useEffect } from "react";
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "GameScene" });
+  }
+
+  preload() {
+    this.load.image("character", "/character.png"); // Place your image in /public
+  }
+
+  create() {
+    this.add.text(100, 50, "Welcome to Himanshu's RPG Portfolio", {
+      font: "20px Arial",
+      fill: "#ffffff",
+    });
+    this.character = this.add.sprite(150, 150, "character").setInteractive();
+    this.character.on("pointerdown", () => alert("Character Selected!"));
+  }
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 400,
+  height: 300,
+  scene: GameScene,
+  parent: "game-container",
+};
+
+const Game = () => {
+  useEffect(() => {
+    new Phaser.Game(config);
+  }, []);
+
+  return <div id="game-container"></div>;
+};
+
+export default Game;
